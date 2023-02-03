73b1c590b51ddc47ca4442df3c2147e6b84c8c27
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-02-03 12:45:24 +0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -8,30 +8,64 @@
     SummerizeShort,
     URLInput,
   } from "./lib/constants";
+  import axios from "axios";
 
   let rawText;
   let url;
   let file;
+  let article;
+
+  let parsingOriginalArticleFromURL;
+  let summerizing;
 
   let summerizedText;
+  $: console.log(summerizedText);
 
   let inputs = [RawTextInput, URLInput, FileInput];
   let input = RawTextInput;
   let summerizeLengths = [SummerizeShort, SummerizeMedium, SummerizeLong];
   let summerizeLength = SummerizeMedium;
 
-  function summerize() {
-    switch (input) {
-      case RawTextInput:
-        summerizedText = rawText;
-        break;
-      case URLInput:
-        summerizedText = url;
-        break;
-      case FileInput:
-        summerizedText = file.name;
-        break;
+  async function getArticle() {
+    if (input == RawTextInput) {
+      return rawText;
     }
+    if (input == URLInput) {
+      parsingOriginalArticleFromURL = true;
+      const res = await axios.post("https://localhost:3001/parsed-from-url", {
+        url,
+      });
+      parsingOriginalArticleFromURL = false;
+      return res.data.text;
+    }
+    if (input == FileInput) {
+      const fr = new FileReader();
+      fr.readAsText(file[0]);
+      fr.onload = () => {
+        return fr.result;
+      };
+    }
+  }
+
+  async function summerize() {
+    article = await getArticle();
+
+    const endpoint = "https://localhost:3001/summerized";
+
+    summerizing = true;
+
+    axios
+      .post(endpoint, {
+        article,
+        length: summerizeLength.toLowerCase(),
+      })
+      .then((res) => (summerizedText = res.data.summerizedArticle))
+      .catch((e) => {
+        alert(e);
+      })
+      .finally(() => {
+        summerizing = false;
+      });
   }
 </script>
 
@@ -56,33 +90,54 @@
     </div>
   </div>
   <div class="flex-1 grid grid-cols-2 gap-4 p-4">
-    <div class="border w-full h-full rounded p-4">
+    <div class="w-full h-full flex flex-col gap-1">
       {#if input == RawTextInput}
+        <p>Enter raw text</p>
         <textarea
           bind:value={rawText}
-          class="bg-white border-0 w-full h-full"
+          class="bg-white rounded border-gray-200 w-full flex-1"
         />
       {:else if input == URLInput}
-        <div class="flex flex-col gap-1">
-          <p>Enter URL</p>
-          <input type="text" bind:value={url} class="border-gray-200 rounded" />
-        </div>
+        <p>Enter URL</p>
+        <input type="text" bind:value={url} class="border-gray-200 rounded" />
+        <p>Parsed article content</p>
+        {#if parsingOriginalArticleFromURL}
+          <div class="w-full h-full flex items-center justify-center">
+            <div
+              class="inline-flex px-3 py-1 bg-gray-100 rounded gap-1 text-xl"
+            >
+              <div class="animate-spin inline-flex">
+                <i class="ri-loader-line" />
+              </div>
+              <div>Parsing article from URL</div>
+            </div>
+          </div>
+        {:else}
+          <textarea
+            bind:value={article}
+            class="bg-white rounded border-gray-200 w-full flex-1"
+            contenteditable="false"
+          />
+        {/if}
       {:else}
-        <div class="flex flex-col gap-1">
-          <p>Upload text file</p>
-          <input bind:files={file} type="file" />
-        </div>
+        <p>Upload text file</p>
+        <input bind:files={file} type="file" />
       {/if}
     </div>
-    <div class="border w-full h-full rounded p-4">{summerizedText}</div>
-
-    <!-- <div class="w-full h-full flex items-center justify-center">
-      <div class="inline-flex px-3 py-1 bg-gray-100 rounded gap-1 text-xl">
-        <div class="animate-spin inline-flex">
-<i class="ri-loader-line"></i>
+    <div class="w-full h-full flex flex-col gap-1">
+      <p>Summerized article</p>
+      {#if summerizing}
+        <div class="w-full h-full flex items-center justify-center">
+          <div class="inline-flex px-3 py-1 bg-gray-100 rounded gap-1 text-xl">
+            <div class="animate-spin inline-flex">
+              <i class="ri-loader-line" />
+            </div>
+            <div>Summerizing</div>
+          </div>
         </div>
-        <div>Summerizing</div>
-      </div>
-    </div> -->
+      {:else}
+        <div class="border w-full h-full rounded p-4">{summerizedText}</div>
+      {/if}
+    </div>
   </div>
 </div>
